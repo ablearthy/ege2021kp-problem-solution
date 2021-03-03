c05579918c0fd6ae171fe0dbbf8789a542e7ba84
Able Arthy <ablearthy@gmail.com>
2021-03-03 19:55:30 +0500

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ege_problem_solution",
+    version="0.0.1",
+    author="Able Arthy",
+    author_email="ablearthy@gmail.com",
+    description="EGE problems solution",
+    packages=find_packages(exclude="tests"),
+)
